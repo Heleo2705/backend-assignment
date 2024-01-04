118324f34b6be904d374dbe311c17b86c04c5eb6
Heleo2705 <heleonidasspartan@gmail.com>
2024-01-05 01:19:58 +0530

    added middlewares, jwt, and handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,17 +5,29 @@ import (
 	"log"
 	"net/http"
 
+	"example.com/backend-assignment/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	godotenv.Load()
+	err := handlers.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
 func main() {
 	mux := chi.NewRouter()
 	v1Router := chi.NewRouter()
 	v1Router.Use(middleware.Logger)
 	v1Router.Use(middleware.Recoverer)
 	// user apis
-	v1Router.Group(func(r chi.Router) {})
+	v1Router.Group(func(r chi.Router) {
+
+	})
 	// notes apis
 	v1Router.Group(func(r chi.Router) {})
 	mux.Mount("/api", v1Router)
